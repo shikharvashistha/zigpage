6651ddb8ad3bdbf9c7ca679d5adf3425f2617bc4
Shikhar Vashistha <51105234+shikharvashistha@users.noreply.github.com>
2023-02-15 10:02:43 +0530

    feat: Notes UI
    This commit introduces UI for Notes Application

--- src/capy.zig
+++ src/capy.zig
@@ -2,24 +2,68 @@ const std = @import("std");
 const capy = @import("capy");
 pub usingnamespace capy.cross_platform;
 
-const Button = capy.Button;
-const Column = capy.Column;
+const ListModel = struct {
+    size: capy.DataWrapper(usize) = capy.DataWrapper(usize).of(10),
+    arena: std.heap.ArenaAllocator = std.heap.ArenaAllocator.init(capy.internal.lasting_allocator),
 
-var computationLabel: capy.Label_Impl = undefined;
-
+    pub fn getComponent(self: *ListModel, index: usize) capy.Button_Impl {
+        //return capy.Label(.{
+        //    .alignment = .Left,
+        //    .text = std.fmt.allocPrintZ(self.arena.allocator(), "File #{d}", .{index + 1}) catch unreachable,
+        //});
+        return capy.Button(.{ .label = std.fmt.allocPrintZ(self.arena.allocator(), "File #{d}", .{index + 1}) catch unreachable });
+    }
+};
 
 pub fn main() !void {
     try capy.backend.init();
+
+    var list_model = ListModel{};
+
     var window = try capy.Window.init();
-    computationLabel = capy.Label(.{ .text = "", .alignment = .Left });
-    try window.set(capy.Column(.{ .expand = .Fill, .spacing = 10 }, .{
-        &computationLabel,
-        capy.Expanded(Column(.{ .expand = .Fill, .spacing = 10 }, .{
-            Button(.{ .label = "7" }),
-        })),
-    }));
-    window.resize(400, 500);
+    try window.set(
+        capy.Stack(
+            .{
+                capy.Rect(.{ .color = capy.Color.comptimeFromString("#000000") }),
+                capy.Column(.{}, .{
+                    capy.Stack(.{
+                        capy.Rect(.{ .color = capy.Color.comptimeFromString("#000000") }),
+                        capy.Label(.{ .text = "Files Opened", .alignment = .Left }),
+                    }),
+                }),
+                capy.Tabs(
+                    .{ capy.Tab(.{ .label = "Files" }, Tab()), capy.Tab(.{ .label = "Edit" }, Tab()), capy.Tab(.{ .label = "View" }, Tab()), capy.Tab(.{ .label = "Run" }, Tab()), capy.Tab(.{ .label = "Help" }, Tab()) },
+                ),
+            },
+        ),
+    );
     window.setTitle("Notes");
     window.show();
+    var last_add = std.time.milliTimestamp();
+    while (capy.stepEventLoop(.Blocking)) {
+        while (std.time.milliTimestamp() >= last_add + 1000) : (last_add += 1000) {
+            list_model.size.set(list_model.size.get() + 1);
+            std.log.info("There are now {} items.", .{list_model.size.get()});
+        }
+        break;
+    }
+
     capy.runEventLoop();
+}
+
+fn Tab() anyerror!capy.Container_Impl {
+    //return capy.Column(.{ .expand = .Fill, .spacing = 10 }, .{
+    //    capy.Label(
+    //        .{ .text = "", .alignment = .Left },
+    //    ),
+    //capy.Expanded(capy.Column(.{ .expand = .Fill, .spacing = 10 }, .{
+    //    capy.Button(.{ .label = "shikhar.cpp" }),
+    //})),
+    //});
+    var list_model = ListModel{};
+    return capy.Stack(
+        .{
+            capy.ColumnList(.{}, &list_model),
+        },
+    );
 }
